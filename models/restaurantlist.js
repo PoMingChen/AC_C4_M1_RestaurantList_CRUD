871cd216eaa16df116afb8b9430456d31e7d8e6e
陳柏銘 <alexchen000003@gmail.com>
2024-09-26 23:37:51 +0800

    update the schema with migrations and also the sequelize model instance to add the 'area' column

--- models/restaurantlist.js
+++ models/restaurantlist.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate (models) {
+    static associate(models) {
       // define association here
     }
   }
@@ -22,7 +22,13 @@ module.exports = (sequelize, DataTypes) => {
     phone: DataTypes.STRING,
     google_map: DataTypes.STRING,
     rating: DataTypes.DECIMAL,
-    description: DataTypes.STRING
+    description: DataTypes.STRING,
+    area:
+    {
+      type: DataTypes.STRING,
+      defaultValue: '臺灣',
+      allowNull: true
+    }
   }, {
     sequelize,
     modelName: 'restaurantlist'
