3f8e46b5276bd05cd65b3eabd838e25798f734cf
陳柏銘 <alexchen000003@gmail.com>
2024-09-28 23:14:39 +0800

    add the detailed logic of sorting when the user select the sorting of category or area

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const db = require('../models')
+const { literal } = require('sequelize');  // Import literal here
 const restaurantList = db.restaurantlist
 
 router.get('/', (req, res) => {
@@ -15,9 +16,34 @@ router.get('/', (req, res) => {
   } else if (sortOption === 'nameDesc') {
     order = [['name', 'DESC']];  // Sort by name in descending order (Z-A)
   } else if (sortOption === 'category') {
-    order = [['category', 'ASC']];  // Sort by name in descending order (Z-A)
+    // order = [['category', 'ASC']];  
+    order = [[literal(`
+    CASE 
+      WHEN category IS NULL THEN 9999
+      WHEN category = '中東料理' THEN 1 
+      WHEN category = '日本料理' THEN 2 
+      WHEN category = '義式餐廳' THEN 3 
+      WHEN category = '美式' THEN 4 
+      WHEN category = '酒吧' THEN 5
+      WHEN category = '咖啡' THEN 6
+      ELSE 9998
+    END ASC
+  `)]];
   } else if (sortOption === 'area') {
-    order = [['area', 'ASC']];  // Sort by name in descending order (Z-A)
+    // order = [['area', 'ASC']];  
+    order = [[literal(`
+    CASE 
+      WHEN area IS NULL THEN 9999
+      WHEN area = '臺北市' THEN 1 
+      WHEN area = '新北市' THEN 2 
+      WHEN area = '桃園市' THEN 3 
+      WHEN area = '臺中市' THEN 4 
+      WHEN area = '臺南市' THEN 5
+      WHEN area = '高雄市' THEN 6
+      WHEN area = '六都以外' THEN 7
+      ELSE 9998
+    END ASC
+  `)]];
   }
 
   // Fetch all restaurants from the database
