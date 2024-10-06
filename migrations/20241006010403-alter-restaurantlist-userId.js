020e302cd05a8c1266f565079388013a5c91c3d5
陳柏銘 <alexchen000003@gmail.com>
2024-10-06 13:01:04 +0800

    Initiated and executed the updated seeder data within the relationshi between 2 default users and 8 restaurants

--- migrations/20241006010403-alter-restaurantlist-userId.js
+++ migrations/20241006010403-alter-restaurantlist-userId.js
@@ -10,7 +10,7 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.changeColumn('restaurantlists ', 'userId', {
+    await queryInterface.changeColumn('restaurantlists', 'userId', {
       type: Sequelize.INTEGER,
       allowNull: true
     })
